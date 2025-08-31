b7290a887ea4c424f44476cfa90cd8133995e9d1
Jeet Patel <52279688+jitupatel2506@users.noreply.github.com>
2025-08-31 21:47:49 +0530

    Update select_crichd_channels.py

--- select_crichd_channels.py
+++ select_crichd_channels.py
@@ -10,7 +10,7 @@ OUTPUT_FILE = "crichd-auto-fetch/auto_crichd_selected_api.json"
 
 # ✅ Array 1: channelName to select from source
 SELECTED_CHANNELS = [
-    "TNT 3",
+    "TNT 4",
     "Star Sports 1",
     "Willow HD",
     "Willow HD",
